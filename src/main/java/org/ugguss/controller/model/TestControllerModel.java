022a45a1a9a8af71ae11c416475a7e092644bc84
Eyuel <eyuel.taddese4@gmail.com>
2018-05-31 21:37:39 -0400

    - Build 001 working  - Added dependency  - Package structure fixed

--- src/main/java/org/ugguss/controller/model/TestControllerModel.java
+++
@@ -1,4 +0,0 @@
-package org.ugguss.controller.model;
-
-public class TestControllerModel {
-}
---
+++ src/main/java/org/ugguss/controllerimpl/ProfilesApiController.java
@@ -0,0 +1,13 @@
+package org.ugguss.controllerimpl;
+
+import org.springframework.stereotype.Controller;
+import org.ugguss.generated.controller.ProfilesApi;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-05-31T21:30:34.195-04:00")
+
+@Controller
+public class ProfilesApiController implements ProfilesApi {
+
+
+
+}
