07ddd548b2dcedbdf990d5b969689679c7a16021
Ivan <Ivan@macbook-pro.local>
2018-06-01 16:29:01 -0500

    Changing main application class name

--- src/main/java/org/ugguss/GussApplication.java
+++ src/main/java/org/ugguss/GussApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class GussApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(GussApplication.class, args);
 	}
 }
