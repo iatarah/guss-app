13256943654a05e2adfb78dacac8bc67ffbab840
Ivan <Ivan@macbook-pro.attlocal.net>
2018-06-10 13:15:32 -0500

    adding classes

--- src/main/java/org/ugguss/repository/IRoleRepository.java
+++ src/main/java/org/ugguss/repository/IRoleRepository.java
@@ -1,8 +1,19 @@
 package org.ugguss.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.ugguss.model.Role;
 
+@Repository
+@Transactional
 public interface IRoleRepository extends JpaRepository<Role, Integer> {
-
+	
+	@Query("FROM Role r WHERE r.roleId = :roleId")
+	Role findRoleByRoleId(@Param("roleId") String roleId);
+	
+	@Query("FROM Role r WHERE r.roleId = :roleName")
+	Role findRoleByRoleName(@Param("roleName") String roleName);
 }
