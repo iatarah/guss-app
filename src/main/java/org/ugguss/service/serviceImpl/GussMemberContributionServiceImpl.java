cff59011e066ca2f8fb7b6b16a2fd4c3b4d33731
Ivan <Ivan@macbook-pro.attlocal.net>
2018-06-20 08:15:10 -0500

    contribution service stubs

---
+++ src/main/java/org/ugguss/service/serviceImpl/GussMemberContributionServiceImpl.java
@@ -0,0 +1,15 @@
+package org.ugguss.service.serviceImpl;
+
+import org.ugguss.generated.model.ContributionRequest;
+import org.ugguss.generated.model.ContributionResponse;
+import org.ugguss.service.IGussMemberContributionService;
+
+public class GussMemberContributionServiceImpl implements IGussMemberContributionService{
+
+	@Override
+	public ContributionResponse createContribution(ContributionRequest contributionRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
