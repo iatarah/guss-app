a7d3b3f12522e019069a3c0b4916ff24eaf14181
Ivan <Ivan@macbook-pro.attlocal.net>
2018-06-24 09:45:58 -0500

    get contribution history api

--- src/main/java/org/ugguss/controllerimpl/MemberContributionApiControllerImpl.java
+++ src/main/java/org/ugguss/controllerimpl/MemberContributionApiControllerImpl.java
@@ -1,16 +1,20 @@
 package org.ugguss.controllerimpl;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.ugguss.generated.controller.MemberContributionApi;
+import org.ugguss.generated.model.ContributionHistoryResponse;
 import org.ugguss.generated.model.ContributionRequest;
 import org.ugguss.generated.model.ContributionResponse;
 import org.ugguss.service.IGussMemberContributionService;
@@ -33,4 +37,13 @@ public class MemberContributionApiControllerImpl implements MemberContributionAp
         return new ResponseEntity<ContributionResponse>(response, HttpStatus.OK);
     }
 
+	
+	@Override
+    @RequestMapping(value = RestEndpointConstants.Constants.BASE_API +"/" + RestEndpointConstants.Constants.MEMBER_CONTRIBUTION,
+    produces = { "application/json" }, 
+    method = RequestMethod.GET)
+    public ResponseEntity<ContributionHistoryResponse> getContribution(@ApiParam(value = "",required=true ) @PathVariable("memberId") String memberId,@ApiParam(value = "") @RequestParam(value = "startDate", required = false) String startDate,@ApiParam(value = "") @RequestParam(value = "endDate", required = false) String endDate) {
+		ContributionHistoryResponse response = iGussMemberContributionService.getContribution(memberId, startDate, endDate);
+        return new ResponseEntity<ContributionHistoryResponse>(response, HttpStatus.OK);
+	}
 }
--- src/main/java/org/ugguss/service/IGussMemberContributionService.java
+++ src/main/java/org/ugguss/service/IGussMemberContributionService.java
@@ -1,9 +1,11 @@
 package org.ugguss.service;
 
+import org.ugguss.generated.model.ContributionHistoryResponse;
 import org.ugguss.generated.model.ContributionRequest;
 import org.ugguss.generated.model.ContributionResponse;
 
 
 public interface IGussMemberContributionService {
 	ContributionResponse createContribution (ContributionRequest contributionRequest);
+	ContributionHistoryResponse getContribution(String memberId, String startDate, String endDate);
 }
