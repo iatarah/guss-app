aebc370d6cdf7b7873187d17293cb74061003cb3
Ivan <Ivan@macbook-pro.attlocal.net>
2018-07-21 05:02:07 -0500

    more work on registration form

--- src/main/webapp/src/app/user-management/user-registration/user-registration.component.ts
+++ src/main/webapp/src/app/user-management/user-registration/user-registration.component.ts
@@ -1,8 +1,7 @@
-import { Gender } from './../../gen/model/gender';
-import { Validators, FormGroupName } from '@angular/forms';
-import { Router } from '@angular/router';
-import { FormBuilder } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
+import { Gender } from './../../gen/model/gender';
+import { Validators, FormGroupName, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -14,31 +13,27 @@ export class UserRegistrationComponent implements OnInit {
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
-  gender: any[] = [
+  userGender: any[] = [
     {value: 'M', viewValue: 'Male'},
     {value: 'F', viewValue: 'Female'}
+  ];
+  appUserRole: any[] = [
+    {value: 'ADMIN', viewValue: 'Administrator'},
+    {value: 'STAFF', viewValue: 'Staff'},
+    {value: 'MEMBER', viewValue: 'Guss Member'}
+  ];
+  appUserStatus: any[] = [
+    {value: 'active', viewValue: 'Active'},
+    {value: 'inactive', viewValue: 'Inactive'}
+  ];
+  memberCategory: any[] = [
+    {value: 'YRA1999', viewValue: 'YRA1999'},
+    {value: 'YRB2000', viewValue: 'YRA1999'}
   ];
   constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstName: ['', Validators.required],
-      middleName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      userRole: ['', Validators.required],
-      status: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      memberId: ['', Validators.required],
-      membershipCategory: ['', Validators.required],
-      joinDate: ['', Validators.required],
-      secondCtrl: ['', Validators.required],
-      retirementDate: ['', Validators.required],
-      basicSalary: ['', Validators.required],
-      address: ['', Validators.required]
-    });
+    this.buildFormColtrols();
   }  
   form1(){
     console.log(this.firstFormGroup.value);
@@ -46,6 +41,27 @@ export class UserRegistrationComponent implements OnInit {
 
   form2(){
     console.log(this.secondFormGroup.value);
+  }
+
+  private buildFormColtrols() {
+    this.firstFormGroup= this._formBuilder.group({
+      firstName: ['', Validators.required],
+      middleName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      userRole: ['', Validators.required],
+      status: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      gender: ['', Validators.required]
+    });
+    this.secondFormGroup= this._formBuilder.group({
+      memberId: ['', Validators.required],
+      membershipCategory: ['', Validators.required],
+      joinDate: ['', Validators.required],
+      retirementDate: ['', Validators.required],
+      basicSalary: ['', Validators.required],
+      address: ['', Validators.required]
+    });
   }
 }
 
