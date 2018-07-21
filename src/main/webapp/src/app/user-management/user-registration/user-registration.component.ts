7d2c1f859c1eafa9da7f503d9b9d1b929d6664b5
Ivan <Ivan@macbook-pro.attlocal.net>
2018-07-21 06:27:08 -0500

    registration page integration with backend service

--- src/main/webapp/src/app/user-management/user-registration/user-registration.component.ts
+++ src/main/webapp/src/app/user-management/user-registration/user-registration.component.ts
@@ -1,3 +1,9 @@
+import { BaseResponse } from './../../gen/dist/model/baseResponse.d';
+import { AppUser } from './../../gen/model/appUser';
+import { BaseRequest } from './../../gen/model/baseRequest';
+import { Member } from './../../gen/dist/model/member.d';
+import { UserRegistrationRequest } from './../../gen/dist/model/userRegistrationRequest.d';
+import { RegistrationService } from './../../gen/api/registration.service';
 import { FormGroup } from '@angular/forms';
 import { Gender } from './../../gen/model/gender';
 import { Validators, FormGroupName, FormBuilder } from '@angular/forms';
@@ -23,18 +29,36 @@ export class UserRegistrationComponent implements OnInit {
     {value: 'MEMBER', viewValue: 'Guss Member'}
   ];
   appUserStatus: any[] = [
-    {value: 'active', viewValue: 'Active'},
-    {value: 'inactive', viewValue: 'Inactive'}
+    {value: '0', viewValue: 'Active'},
+    {value: '1', viewValue: 'Inactive'}
   ];
   memberCategory: any[] = [
     {value: 'YRA1999', viewValue: 'YRA1999'},
     {value: 'YRB2000', viewValue: 'YRA1999'}
   ];
-  constructor(private _formBuilder: FormBuilder) {}
+  private userRegistrationRequest: UserRegistrationRequest;
+  private appUser: AppUser;
+  private gussMember: Member;
+  baseResponse: BaseResponse;
+  constructor(private _formBuilder: FormBuilder, private registrationService: RegistrationService) {}
 
   ngOnInit() {
     this.buildFormColtrols();
-  }  
+
+
+  } 
+  onSubmit() {
+        // calling service
+        this.userRegistrationRequest = this.buildUserRegistrationRequest(this.firstFormGroup.value, this.secondFormGroup.value);
+        this.registrationService.registerUser(this.userRegistrationRequest).subscribe(data => {
+          this.appUser = data.appUser;
+          this.gussMember = data.gussMember;
+          this.baseResponse = data.baseResponse;
+          console.log("In method now >>>>>>>>>>");
+          console.log(this.baseResponse);
+        });
+  }
+
   form1(){
     console.log(this.firstFormGroup.value);
   }
@@ -42,7 +66,6 @@ export class UserRegistrationComponent implements OnInit {
   form2(){
     console.log(this.secondFormGroup.value);
   }
-
   private buildFormColtrols() {
     this.firstFormGroup= this._formBuilder.group({
       firstName: ['', Validators.required],
@@ -62,6 +85,21 @@ export class UserRegistrationComponent implements OnInit {
       basicSalary: ['', Validators.required],
       address: ['', Validators.required]
     });
+  }
+  public buildUserRegistrationRequest(form1: any, form2: any): UserRegistrationRequest {
+    let userRegistrationRequest: UserRegistrationRequest = {
+      baseRequest: null,
+      appUser: null,
+      member: null
+    };
+    let baseRequest: BaseRequest;
+    let appUser: AppUser = form1;
+    let member: Member = form2;
+
+    userRegistrationRequest.appUser = appUser;
+    userRegistrationRequest.member = member;
+    userRegistrationRequest.baseRequest = null;
+    return userRegistrationRequest;
   }
 }
 
