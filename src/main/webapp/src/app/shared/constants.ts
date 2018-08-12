0e462bcaed253185702d07f6022fa1488d725305
Ivan <Ivan@macbook-pro.attlocal.net>
2018-08-11 19:40:11 -0500

    adding constants

---
+++ src/main/webapp/src/app/shared/constants.ts
@@ -0,0 +1,11 @@
+export class AppConstants {
+    
+    // messages
+    public static MSG_SYSTEM_ERROR='System error. Please contact your system administrator';
+    public static MSG_SUCCESS='Success!!';
+
+    // constants
+    public static CONST_SUCCESS='success';
+    public static CONST_ERROR='error';
+
+ }
