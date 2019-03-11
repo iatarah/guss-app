2a7ae64c7b298a29e1539c555398d02a758d6ed0
Ivan <Ivan@macbook-pro.attlocal.net>
2019-03-11 13:50:32 -0500

    prod changes

--- src/main/webapp/src/app/shared/_services/token_interceptor.service.ts
+++ src/main/webapp/src/app/shared/_services/token_interceptor.service.ts
@@ -6,21 +6,21 @@ import { AuthService } from "./auth.service";
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
     private blackList = [
-        'http://localhost:8080/guss-app/rest/ugguss/api/v1/token/auth'
+        'http://18.220.11.193:8080/guss-app/rest/ugguss/api/v1/token/auth'
     ]
-    
-    constructor(private authServiceImpl : AuthService) {}
+
+    constructor(private authServiceImpl: AuthService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log(request.urlWithParams);
         console.log("Interceptor blacklist");
         // If request is not in whitlist add header
-        if(!this.blackList.includes(request.url)) {
+        if (!this.blackList.includes(request.url)) {
             console.log("I was not here");
             console.log(request.url);
-            let myToken : string = localStorage.getItem('currentUser');
+            let myToken: string = localStorage.getItem('currentUser');
             let isAuthenticated = this.authServiceImpl.isAuthenticated();
-            if(isAuthenticated && myToken != null) {
+            if (isAuthenticated && myToken != null) {
                 console.log(myToken);
                 console.log("Am in interceptor");
                 request = request.clone({
